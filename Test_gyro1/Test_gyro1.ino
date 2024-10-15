beac76ad35eb47cc8f09eff8789117fb322f50df
Youri <youri.hyjou@gmail.com>
2024-10-15 11:15:48 +0200

    millis test + comments

--- Test_gyro1/Test_gyro1.ino
+++ Test_gyro1/Test_gyro1.ino
@@ -3,12 +3,12 @@
 #include <SoftwareSerial.h>
 #include "Servo.h"
 #include <I2Cdev.h>
-//un vieux commentaire
+
 Servo servo; // création de l'objet "servo"
 const int servoPin = 6; //définition du pin du servo
 
 
-//definition variable pour la lecture angle y
+//Définition variable pour la lecture angle y
 const int MPU_addr=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 int minVal=265;
@@ -17,12 +17,12 @@ double x;
 double y;
 double z;
 
-//définition des variables pour le système d'éclairage
+//Définition des variables pour le système d'éclairage
 const int PIN_ENABLE_LEDS = 3; //PIN pour la commande de l'éclairage
 int CHOIX_MODE = 0000; // 1000 = éclairage --  2000 = defense
 const int frequence_clignotement = 50; //En ms
 
-//definition variable pour asservissement et correcteur PID
+//Définition variable pour asservissement et correcteur PID
 const double Consigne = 0.0; //consigne à atteindre
 double prevError = 0.0;
 double I = 0.0;
@@ -31,11 +31,16 @@ const double Ki = 0.1;
 const double Kd = 1.0;
 int mode_camera = 21; //Mode automatique = 21 - Mode manuel = 31 |
 
-//définition des variables pour le bluetooth
+//Définition des variables pour le bluetooth
 const int RX = 1;
 const int TX = 0;
 SoftwareSerial bluetoothSerial(RX, TX); // RX, TX
 
+//Définition des périodes et variables de temps
+unsigned long currentMillis = millis();   // Temps actuel
+const unsigned long periodServo = 500;         // Intervalle de 500 ms
+const unsigned long periodGyro = 500;
+const unsigned long periodTension = 5000;
 
 void setup() {
   Wire.begin();
@@ -45,9 +50,11 @@ void setup() {
   Wire.endTransmission(true);
   Serial.begin(9600);
 
+  //Définition servo
   servo.attach(servoPin); // attache le servo au pin spécifié
   servo.write(Consigne); //définir la position initiale du servo
 
+  //Définition 
   pinMode (PIN_ENABLE_LEDS, OUTPUT); //définition du PIN ENABLE LED comme une sortie
   analogWrite(PIN_ENABLE_LEDS, 0); //on éteind l'éclairage par défaut
 
@@ -63,7 +70,7 @@ void loop() {
   CHOIX_MODE = (int)bluetoothSerial.read(); //Lecture de la commande bluetooth
   //testservo();
   choixModeEclairage(CHOIX_MODE);
-  choixModeCamera(choixModeCamera);
+  choixModeCamera(mode_camera);
 }
 
 
@@ -71,31 +78,42 @@ void loop() {
 //                                    Fonction : Asservissement servomoteur
 //-----------------------------------------------------------------------------------------------------------------
 void asservissement_servo(double angleY){
+  static unsigned long previousMillis = 0;
+  if(currentMillis - previousMillis >= periodServo) {
+    previousMillis = currentMillis;
+         // Calcul de l'erreur
+    double error = Consigne - angleY;
+    Serial.print("Error = ");
+    Serial.println(error);
+    // Calcul des termes PID
+    double P = Kp * error;
+    if (-3 < error < 3) {
+    I += Ki * error;
+    }
+    double D = Kd * (error - prevError)/periodServo;
   
-  // Calcul de l'erreur
-  double error = Consigne - angleY;
-
-  // Calcul des termes PID
-  double P = Kp * error;
-  I += Ki * error;
-  double D = Kd * (error - prevError);
-
-  // Calcul de la commande du servomoteur
-  double servoCommand = P + I + D;
-
-  // Limiter la commande du servomoteur dans la plage acceptable
-  servoCommand = constrain(servoCommand, 0, 180);
-
-  // Envoyer la commande au servomoteur
-  servo.write(servoCommand);
-
-  // Mise à jour de l'erreur précédente
-  prevError = error;
-
-  delay(20);  // Ajustez si nécessaire pour la fréquence de mise à jour
+    // Calcul de la commande du servomoteur
+    double servoCommand = P + I + D;
+    servoCommand = map(servoCommand, -150, 150, -50, 50);
+    Serial.print("servoCommand = ");
+    Serial.println(servoCommand);
+    // Limiter la commande du servomoteur dans la plage acceptable
+    //servoCommand = constrain(servoCommand, 0, 180);
+  
+    // Envoyer la commande au servomoteur
+    servoCommand = servoCommand+angleY;
+    Serial.print("servoCommand = ");
+    Serial.println(servoCommand);
+    servo.write(servoCommand);
+  
+    // Mise à jour de l'erreur précédente
+    prevError = error;
+  }
 }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
+
+
 
 //-----------------------------------------------------------------------------------------------------------------
 //                                    Fonction : Test du servomoteur
@@ -120,31 +138,35 @@ void testservo() {
 //-----------------------------------------------------------------------------------------------------------------
 double lire_angle_gyro( ) { 
   //a vérifier
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true);
-
-  //lecture des angles
-  AcX=Wire.read()<<8|Wire.read();
-  AcY=Wire.read()<<8|Wire.read();
-  AcZ=Wire.read()<<8|Wire.read();
-  int xAng = map(AcX,minVal,maxVal,-90,90);
-  int yAng = map(AcY,minVal,maxVal,-90,90);
-  int zAng = map(AcZ,minVal,maxVal,-90,90);
-
-  //conversion des angles en degrés
-  x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
-  y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
-  z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
-   
-  //renvoi de la valeur de l'angle y
-  Serial.println("AngleY= ");
-  Serial.print(y);
-  Serial.println("-----------------------------------------");
-
-  return(y);
-  delay(800);
+  static unsigned long previousMillis = 0;
+  if(currentMillis - previousMillis >= periodGyro) {
+    previousMillis = currentMillis;
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);
+  
+    //lecture des angles
+    AcX=Wire.read()<<8|Wire.read();
+    AcY=Wire.read()<<8|Wire.read();
+    AcZ=Wire.read()<<8|Wire.read();
+    int xAng = map(AcX,minVal,maxVal,-90,90);
+    int yAng = map(AcY,minVal,maxVal,-90,90);
+    int zAng = map(AcZ,minVal,maxVal,-90,90);
+  
+    //conversion des angles en degrés
+    x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+    y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+    z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+     
+    //renvoi de la valeur de l'angle y
+    Serial.println("AngleY= ");
+    Serial.print(y);
+    Serial.println("-----------------------------------------");
+  
+    return y;
+  }
+  return -1;
 }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
@@ -154,30 +176,32 @@ double lire_angle_gyro( ) {
 //                                    Fonction : Lire la tension pour la sécu. batterie
 //-----------------------------------------------------------------------------------------------------------------
 void readTension( ) {
-  digitalWrite(4, HIGH); // Définit la broche 4 en état HIGH
-  delay(100);
-  digitalWrite(4, LOW); // Définit la broche 4 en état LOW
-  float sommeTension = 0.0; //définition pour faire la moyenne de la tension
-  int echantillon=1000; //A MODIFIER, valeur du nombre d'echantillon qui définie la précision de notre mesure
-  for (int i = 0; i < echantillon; i++) { //on récupère toutes les valeurs selon le nombre d'échantillon désiré
-    int valeurTension = analogRead(A0);
-    float tension = (float)valeurTension * 5.0 / 1023.0; //on mutlplie par 5/1023 pour obtenir la valeur en tension, l'arduino étant codé sur 1024 bits
-    sommeTension += tension; 
-    
-    delay(100/echantillon); // Attendre un peu entre chaque lecture en fonction du nombre d'échantillon (10 secondes)
-  }
+  static unsigned long previousMillis = 0;
+  if(currentMillis - previousMillis >= periodTension) {
+    //digitalWrite(4, HIGH); // Définit la broche 4 en état HIGH
+    //delay(100);
+    //digitalWrite(4, LOW); // Définit la broche 4 en état LOW
+    float sommeTension = 0.0; //définition pour faire la moyenne de la tension
+    int echantillon=1000; //A MODIFIER, valeur du nombre d'echantillon qui définie la précision de notre mesure
+    for (int i = 0; i < echantillon; i++) { //on récupère toutes les valeurs selon le nombre d'échantillon désiré
+      int valeurTension = analogRead(A0);
+      float tension = (float)valeurTension * 5.0 / 1023.0; //on mutlplie par 5/1023 pour obtenir la valeur en tension, l'arduino étant codé sur 1024 bits
+      sommeTension += tension; 
+      
+      delay(100/echantillon); // Attendre un peu entre chaque lecture en fonction du nombre d'échantillon (10 secondes)
+    }
+  
+    // Calculer la moyenne des échantillons
+    float moyenneTension = sommeTension / echantillon;
+  
+    // Envoi de la moyenne via Bluetooth
+    Serial.println("Moyenne tension (");
+    Serial.print(echantillon);
+    Serial.print(" echantillons): ");
+    Serial.print(moyenneTension, 2); // Affiche la moyenne avec 2 décimales
+    Serial.println(" V");
+    }
 
-  // Calculer la moyenne des échantillons
-  float moyenneTension = sommeTension / echantillon;
-
-  // Envoi de la moyenne via Bluetooth
-  Serial.println("Moyenne tension (");
-  Serial.print(echantillon);
-  Serial.print(" echantillons): ");
-  Serial.print(moyenneTension, 2); // Affiche la moyenne avec 2 décimales
-  Serial.println(" V");
-
-  delay(100); // Attendez  0.1 seconde avant de lire à nouveau la valeur
 }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
