77977abaa4d24c1097c25d2b671933ef19006951
Youri <youri.hyjou@gmail.com>
2024-12-10 13:04:55 +0100

    asservissement_casi fini

--- Test_gyro1/Test_gyro1.ino
+++ Test_gyro1/Test_gyro1.ino
@@ -16,16 +16,26 @@ int maxVal=402;
 double x;
 double y;
 double z;
-
-
+double Consigne;
+unsigned long currentMillis = millis();   // Temps actuel  
+double angleMesure;
+double angleConsigne = 90;  
+double previousAngle = 90; //notre zero est 90 sur le gyro
+double DeadZoneGyro = 3; //on laisse une marge de 3 degré pour que le servo bouge
+//Variable asserv
+double prevError = 0.0;
+double I = 0;
+const double Kp = 1;
+const double Ki = 1;
+const double Kd = 0.5;
 //Définition des variables pour le bluetooth
 //const int RX = 1;
 //const int TX = 0;
 //SoftwareSerial bluetoothSerial(RX, TX); // RX, TX
 
 //Définition des périodes et variables de temps
-unsigned long currentMillis = millis();   // Temps actuel    
-const unsigned long periodGyro = 500;
+const unsigned long periodGyro = 50;
+const unsigned long periodServo = 100;
 
 void setup() {
   //Setup gyroscope
@@ -34,10 +44,11 @@ void setup() {
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
+  
 
   //Définition servo
   servo.attach(servoPin); // attache le servo au pin spécifié
-  servo.write(Consigne); //définir la position initiale du servo
+  servo.write(90); //définir la position initiale du servo
 
   Serial.begin(9600);
 
@@ -48,41 +59,33 @@ void setup() {
 }
 
 void loop() {
-
-  //testservo();
-  choixModeEclairage(CHOIX_MODE);
-  choixModeCamera(mode_camera);
+  lire_angle();
+  //Serial.println(angleMesure);
+  //Serial.println(angleConsigne - abs(angleConsigne-angleMesure));
+  //servo.write(angleConsigne - abs(angleConsigne-angleMesure));
+  asservissement_servo(angleMesure);
 }
-
 
 //-----------------------------------------------------------------------------------------------------------------
 //                                    Fonction : Asservissement servomoteur
 //-----------------------------------------------------------------------------------------------------------------
 void asservissement_servo(double angleY){
-  static unsigned long previousMillis = 0;
-  if(currentMillis - previousMillis >= periodServo) {
-    previousMillis = currentMillis;
+  unsigned long currentMillisAsserv = millis(); 
+  static unsigned long previousMillisAsserv = 0;
+  if(currentMillisAsserv - previousMillisAsserv >= periodServo) {
+    previousMillisAsserv = currentMillisAsserv; 
          // Calcul de l'erreur
-    double error = Consigne - angleY;
+    double error = angleConsigne - angleY;
     Serial.print("Error = ");
     Serial.println(error);
     // Calcul des termes PID
     double P = Kp * error;
-    if (-3 < error < 3) {
     I += Ki * error;
-    }
     double D = Kd * (error - prevError)/periodServo;
   
     // Calcul de la commande du servomoteur
     double servoCommand = P + I + D;
-    servoCommand = map(servoCommand, -150, 150, -50, 50);
-    Serial.print("servoCommand = ");
-    Serial.println(servoCommand);
-    // Limiter la commande du servomoteur dans la plage acceptable
-    //servoCommand = constrain(servoCommand, 0, 180);
-  
-    // Envoyer la commande au servomoteur
-    servoCommand = servoCommand+angleY;
+    servoCommand = map(servoCommand, -150, 150, 150, 30);
     Serial.print("servoCommand = ");
     Serial.println(servoCommand);
     servo.write(servoCommand);
@@ -95,7 +98,6 @@ void asservissement_servo(double angleY){
 //-----------------------------------------------------------------------------------------------------------------
 
 
-
 //-----------------------------------------------------------------------------------------------------------------
 //                                    Fonction : Test du servomoteur
 //-----------------------------------------------------------------------------------------------------------------
@@ -103,25 +105,35 @@ void testservo() {
    servo.write(0); // demande au servo de se déplacer à cette position
    delay(1000); // attend 1000 ms entre changement de position
 
-   servo.write(90); // demande au servo de se déplacer à cette position
+   servo.write(15); // demande au servo de se déplacer à cette position
    delay(1000); // attend 1000 ms entre changement de position
 
-   servo.write(180); // demande au servo de se déplacer à cette position
+   servo.write(30); // demande au servo de se déplacer à cette position
    delay(1000); // attend 1000 ms entre changement de position
 }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
 
-
+void lire_angle() {
+  static unsigned long previousMillisGyro = 0;
+  unsigned long currentMillisGyro = millis(); 
+  if(currentMillisGyro - previousMillisGyro >= periodGyro) {
+    previousMillisGyro = currentMillisGyro; 
+    if(abs(angleConsigne - lire_angle_gyro()) > DeadZoneGyro) {  
+    angleMesure = lire_angle_gyro();
+    previousAngle = angleMesure;
+    }
+  }
+  else {
+    angleMesure = previousAngle;
+  }  
+}
 
 //-----------------------------------------------------------------------------------------------------------------
 //                                    Fonction : Capter la position avec le gyroscope
 //-----------------------------------------------------------------------------------------------------------------
-double lire_angle_gyro( ) { 
-  //a vérifier
-  static unsigned long previousMillis = 0;
-  if(currentMillis - previousMillis >= periodGyro) {
-    previousMillis = currentMillis;
+double lire_angle_gyro() { 
+    //lecture des angles
     Wire.beginTransmission(MPU_addr);
     Wire.write(0x3B);
     Wire.endTransmission(false);
@@ -141,13 +153,9 @@ double lire_angle_gyro( ) {
     z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
      
     //renvoi de la valeur de l'angle y
-    Serial.println("AngleY= ");
-    Serial.print(y);
-    Serial.println("-----------------------------------------");
-  
-    return y;
-  }
-  return -1;
+    //Serial.println("AngleY= ");
+    //Serial.println("-----------------------------------------");
+    return y+10;
 }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
