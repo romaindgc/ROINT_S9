1810e7c914eabfc2115b8173361b27d748c867df
Youri <youri.hyjou@gmail.com>
2024-12-17 14:52:21 +0100

    ajout gyro et camera dans le main

--- Main/main/main.ino
+++ Main/main/main.ino
@@ -49,6 +49,37 @@ const int lowerBound_joy1 = Y_joy1 - ceil(1023*0.01);
 const int upperBound_joy2 = Y_joy2 + ceil(1023*0.01);
 const int lowerBound_joy2 = Y_joy2 - ceil(1023*0.01);
 
+
+
+//GYROSCOPE : Définition variable pour la lecture angle y
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int minVal=265;
+int maxVal=402;
+double x;
+double y;
+double z;
+double angleMesure;
+double DeadZoneGyro = 3; //on laisse une marge de 3 degré pour que le servo bouge
+const unsigned long periodGyro = 10;
+//ASSERVISSEMENT : Définition des variables
+bool asservissement_state = false;
+Servo servo; // création de l'objet "servo"
+const int servoPin = 6; //définition du pin du servo
+double angleConsigne = 90;  
+double previousAngle = 90; //notre zero est 90 sur le gyro
+double prevError = 0.0;
+double I = 0;
+const double Kp = 0.03;
+const double Ki = 0.1;
+const double Kd = 0.1;
+double DeadZoneAsserv = 5;
+const unsigned long periodServo = 10;
+
+//CAMERA: Mode manuel définition des variables
+const unsigned long periodCamera = 300; 
+
+
 void setup() {
   //Serial.begin(9600);
   
@@ -69,6 +100,17 @@ void setup() {
   pinMode(PIN_JOY2, INPUT);
 
   miseZero();
+
+  //GYROSCOPE : Setup
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  
+  //ASSERVISSEMENT : Setup
+  servo.attach(servoPin); // attache le servo au pin spécifié
+  servo.write(90); //définir la position initiale du servo
 }
 
 void loop() {
@@ -151,6 +193,13 @@ void loop() {
     Serial.print("Defense actif");
     eclairageDefense();
   }
+
+  //Mode camera automatique 
+  //Si le defense mode est actif, on fait clignoter les LEDs à une certaine fréquence
+  if(asservissement_state){
+    lire_angle();
+    asservissement_servo(angleMesure);
+  }
 }
 
  
@@ -182,6 +231,10 @@ void choixCible(int prefix, int value[]){
     Serial.println("Defense");
     // Choix d'activer le mode défense
     defense_state = true;
+    } else if (prefix == 22) {
+    Serial.println("Asservissement");
+    asservissement_state = false;
+    camera_manual(value[0]);
   } else {
     Serial.println("MaZ");
     miseZero();
@@ -196,8 +249,10 @@ void choixCible(int prefix, int value[]){
 //-----------------------------------------------------------------------------------------------------------------
   void miseZero(){
     defense_state = false; //Mode defense non actif
+    asservissement_state = true; 
     lampeOnOff(0); //On éteint la lampe
     setupMotors();
+
   }
 //-----------------------------------------------------------------------------------------------------------------
 //-----------------------------------------------------------------------------------------------------------------
@@ -325,3 +380,105 @@ bool isInteger(const String &str) {
     return true;
 }
 
+
+//-----------------------------------------------------------------------------------------------------------------
+//                                    Fonction : Mode manuel camera
+//-----------------------------------------------------------------------------------------------------------------
+void camera_manual(int value) {
+  unsigned long currentMillisCamera = millis(); 
+  static unsigned long previousMillisCamera = 0;
+  double angleCameraConsigne = value;
+  double previousAngle = 90; 
+  angleCameraConsigne = map(angleCameraConsigne, 0, 1024, 0, 180); 
+  Serial.println(angleCameraConsigne);  
+  if(currentMillisCamera - previousMillisCamera >= periodCamera) {
+    previousMillisCamera = currentMillisCamera; 
+    servo.write(180-angleCameraConsigne);
+  }
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+//                                    Fonction : Asservissement servomoteur
+//-----------------------------------------------------------------------------------------------------------------
+void asservissement_servo(double angleY){
+  unsigned long currentMillisAsserv = millis(); 
+  static unsigned long previousMillisAsserv = 0;
+  if(currentMillisAsserv - previousMillisAsserv >= periodServo) {
+    previousMillisAsserv = currentMillisAsserv; 
+    Serial.print("AngleY=");
+    Serial.println(angleY);
+    Serial.print("Angleconsigne=");
+    Serial.println(angleConsigne);
+    double error = angleConsigne - angleY;
+    Serial.print("Error = ");
+    Serial.println(error);
+    if(abs(error) > DeadZoneAsserv) { 
+      // Calcul des termes PID
+      double P = Kp * error;
+      I += Ki * error;
+      double D = Kd * (error - prevError)/periodServo;
+      I = constrain(I, -90, 90); //il ne faut pas que I augmente trop -> "windup"
+      // Calcul de la commande du servomoteur
+      Serial.print("I = ");
+      Serial.println(I);
+      double servoCommand = P + I + D;
+      Serial.print("servoCommand = ");
+      Serial.println(servoCommand);
+      servoCommand = map(servoCommand, -90, 90, 0, 180);
+      double servoCommandConsigne = servoCommand;
+      servoCommandConsigne = constrain(servoCommand, 0, 180);
+      Serial.print("servoCommandConsigne = ");
+      Serial.println(servoCommandConsigne);
+      servo.write(180-servoCommandConsigne);
+    
+      // Mise à jour de l'erreur précédente
+      prevError = error;
+     }
+  }
+}
+//-----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
+//                                    Fonctions : Capter la position avec le gyroscope
+//-----------------------------------------------------------------------------------------------------------------
+void lire_angle() {
+  static unsigned long previousMillisGyro = 0;
+  unsigned long currentMillisGyro = millis(); 
+  if(currentMillisGyro - previousMillisGyro >= periodGyro) {
+    previousMillisGyro = currentMillisGyro; 
+    if(abs(angleConsigne - lire_angle_gyro()) > DeadZoneGyro) {  
+      angleMesure = lire_angle_gyro();
+      previousAngle = angleMesure;
+    }
+  }
+  else {
+    angleMesure = previousAngle;
+  }  
+}
+
+double lire_angle_gyro() { 
+    //lecture des angles
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_addr,14,true);
+  
+    //lecture des angles
+    AcX=Wire.read()<<8|Wire.read();
+    AcY=Wire.read()<<8|Wire.read();
+    AcZ=Wire.read()<<8|Wire.read();
+    int xAng = map(AcX,minVal,maxVal,-90,90);
+    int yAng = map(AcY,minVal,maxVal,-90,90);
+    int zAng = map(AcZ,minVal,maxVal,-90,90);
+  
+    //conversion des angles en degrés
+    x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+    y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+    z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+     
+    //renvoi de la valeur de l'angle y
+    //Serial.println("AngleY= ");
+    //Serial.println("-----------------------------------------");
+    return y+10;
+}
+//-----------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------
